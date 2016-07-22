0de06c148f06c91b9768bf99cb974717021a3395
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-07-22 16:15:48 +0530

    Adding the for test.

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,14 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	i:=0
+	for n != 1 {
+		if n%2==0 {
+			n = n/2
+		}else{
+			n = 3*n + 1
+		}
+		i++
+	}
+	return i
 }
